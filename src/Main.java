73695b11841dfb057514e6c8c4c14e9b52e99b96
yanuk88 <yanuk88@gmail.com>
2022-05-02 17:20:56 +0800

    переделано

--- src/Main.java
+++ src/Main.java
@@ -13,29 +13,69 @@ public class Main {
 
         //exercise 2
 
-        System.out.print(numbers[0] + ", " + numbers[1] + ", " + numbers[2]);
+        for (int i = 0; i < numbers.length; i++ ) {
+            if (i != numbers.length - 1) {
+                System.out.print(numbers[i] + ", ");}
+            else {System.out.print(numbers[i]);}
+        }
         System.out.println();
-        System.out.print(numbers1[0] + ", " + numbers1[1] + ", " + numbers1[2]);
+
+        for (int i = 0; i < numbers1.length; i++ ) {
+            if (i != numbers1.length - 1) {
+                System.out.print(numbers1[i] + ", ");}
+            else {System.out.print(numbers1[i]);}
+        }
         System.out.println();
-        System.out.print(mine[0] + ", " + mine[1] + ", " + mine[2] + ", " + mine[3]);
+
+        for (int i = 0; i < mine.length; i++ ) {
+            if (i != mine.length - 1) {
+                System.out.print(mine[i] + ", ");}
+            else {System.out.print(mine[i]);}
+        }
         System.out.println();
 
         //exercise 3
 
-        System.out.print(numbers[2] + ", " + numbers[1] + ", " + numbers[0]);
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(numbers[i] + ", ");}
+            else {
+                System.out.print(numbers[i]);}
+            }
         System.out.println();
-        System.out.print(numbers1[2] + ", " + numbers1[1] + ", " + numbers1[0]);
+
+        for (int i = numbers1.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(numbers1[i] + ", ");}
+            else {
+                System.out.print(numbers1[i]);}
+        }
         System.out.println();
-        System.out.print(mine[3] + ", " + mine[2] + ", " + mine[1] + ", " + mine[0]);
+
+        for (int i = mine.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(mine[i] + ", ");}
+            else {
+                System.out.print(mine[i]);}
+        }
+        System.out.println();
+
 
         //exercise 4
 
-        for (int i = 0; i <= 3; i++) {
-            if(i %2 != 0) {i = i + 1;}
-            System.out.println(i);
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] %2 != 0) {
+                System.out.println(numbers[i] + 1);
+                numbers[i]++;
+            }
+            else {
+                System.out.println(numbers[i]);}
+
+            }
+        }
+
             
         }
 
 
-    }
-}
+
